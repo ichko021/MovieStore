ac386098531658965c68f47ed351a5cf78da4f56
ichko021 <ichko021@abv.bg>
2024-11-10 12:07:29 +0200

    Movie Repository

---
+++ MovieStore/MovieStore.DL/StaticData/StaticDB.cs
@@ -0,0 +1,30 @@
+﻿using MovieStore.Models.DTO;
+
+namespace MovieStore.DL.StaticData
+{
+    internal static class StaticDB
+    {
+
+
+        public static List<Movie> Movies { get; set; } = new List<Movie>() { 
+            new Movie()
+            {
+                Id = 1,
+                Title = "Test1",
+                Year = 2016,
+            },
+            new Movie()
+            {
+                Id = 2,
+                Title = "Test2",
+                Year = 2017,
+            },
+            new Movie()
+            {
+                Id = 3,
+                Title = "Test3",
+                Year = 2018,
+            }
+        }; 
+    }
+}
